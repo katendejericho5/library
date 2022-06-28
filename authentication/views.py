f3315af6fc1cea62cafe7ce0c5bd1f0c315e8709
ddamuliratina <ddamuliratina@gmail.com>
2022-06-28 21:50:09 +0300

    edit of the previous code

--- authentication/views.py
+++ authentication/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'authentication/home.html')
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
